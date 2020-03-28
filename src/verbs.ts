b124e9ac04c30cae851508468e604708315c42d8
Jan Skowronski <crianonim@gmail.com>
2020-03-28 20:08:08 +0000

    verbs

--- src/verbs.ts
+++ src/verbs.ts
@@ -1,4 +1,6 @@
 import * as Schema from './schema';
-
+interface Verb {
+    (ctx: Schema.Dialog, ...arg: any): any
+}
 export const DEBUG = (ctx: Schema.Context, mesage: string) => { console.log("DEBUG:", mesage); return '' }
 export const CURRENT_DIALOG = (ctx: Schema.Context) => ctx.dialogName;
