9cedd03af6aa53ebfa270572994059ea0de42357
Jan Skowronski <crianonim@gmail.com>
2020-03-29 08:23:40 +0100

    screept corrected

--- src/screept.ts
+++ src/screept.ts
@@ -1,12 +1,13 @@
 import * as Schema from './schema';
 
 export function run(text: string, ctx: Schema.Context, verbs: any) {
-    text = text.replace(/\$(\w+\W?)/g, " ctx.$1");
+    console.log("VERBS", verbs)
+    text = text.replace(/\$(\w+\W?)\(/g, " verbs.$1(ctx, ");
     console.log("EVAL", text)
     return eval(text);
 }
 
-export function interpolate(s: string, ctx: Schema.Context, verbs) {
+export function interpolate(s: string, ctx: Schema.Context, verbs: any) {
     return s.replace(/{{(.*?)}}/g, (full, scr) => {
         let result = run(scr, ctx, verbs)
         console.log("RESULT of interpolation", result)
