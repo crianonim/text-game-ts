2d1a2df1df770534dc237e9e2e59a1735cb8367b
Jan Skowronski <crianonim@gmail.com>
2020-03-30 07:35:06 +0100

    split context to separate file

---
+++ src/game-data/context.ts
@@ -0,0 +1,11 @@
+import * as Schema from './../schema';
+
+export const ctx: Schema.Context = {
+    dialogName: 'init',
+    stack: [],
+    turn: 2,
+    options: true,
+    flags: {
+        seen: 0
+    },
+}
