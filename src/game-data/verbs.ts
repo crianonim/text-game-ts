7f9413f1dc1e5adedd5cd827d555df64046ec32b
Jan Skowronski <crianonim@gmail.com>
2020-03-30 07:34:38 +0100

    split verbs into subfiles

---
+++ src/game-data/verbs.ts
@@ -0,0 +1,6 @@
+import * as main from './verbs/verbs_main';
+import * as secondary from './verbs/verbs_secondary';
+
+const verbs = { ...main, ...secondary }
+
+export default verbs
---
+++ src/game-data/verbs/verbs_secondary.ts
@@ -0,0 +1,4 @@
+import * as Schema from '../../schema';
+
+
+export const ADD = (ctx: Schema.Context, v1: number, v2: number) => v1 + v2
